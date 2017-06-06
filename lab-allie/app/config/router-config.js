47df4a0a8ae433c0784e38bf81e79eb71ebb1105
Allie Grampa <agrampa@yahoo.com>
2017-06-05 18:26:24 -0700

    added code from demo

---
+++ lab-allie/app/config/router-config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = [
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    // $urlRouterProvider.when('', '/join#signup'); from lecture
+    $urlRouterProvider.when('', '/home'); // from demo code
+    $urlRouterProvider.when('/', '/join#signup');
+    $urlRouterProvider.when('/signup', '/join#signup');
+    $urlRouterProvider.when('/login', '/join#login');
+    
+    let routes = [
+      {
+        name: 'home',
+        url: '/home',
+        template: require('../view/home/home.html'),
+        controller: 'HomeController',
+        controllerAs: 'homeCtrl',
+      },
+      {
+        name: 'landing',
+        url: '/join',
+        template: require('../view/landing/landing.html'),
+        controller: 'LandingController',
+        controllerAs: 'landingCtrl',
+      },
+    ];
+    routes.forEach(route => $stateProvider.state(route));
+  },
+];
