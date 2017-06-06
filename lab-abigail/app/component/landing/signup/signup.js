af436cea6d4e63215f8f3f55afe7b219cdbcbb82
Abigail White <abigailwhite213@gmail.com>
2017-06-06 11:09:13 -0700

    functional MVP and tests

---
+++ lab-abigail/app/component/landing/signup/signup.js
@@ -0,0 +1,30 @@
+'use strict'
+
+require('./_signup.scss');
+
+module.exports = {
+  template: require('./signup.html'),
+  controller: ['$log', '$location', '$window', 'authService', SignupController],
+  controllerAs: 'signupCtrl'
+}
+
+function SignupController($log, $location, $window, authService) {
+  this.$onInit = () => {
+    $log.debug('SignupController');
+    $log.log('inside SignupController')
+    if(!$window.localStorage.token) {
+              authService.getToken()
+              .then(
+                () => $location.url('/home'),
+                () => $location.url('/signup')
+              )
+            }
+    // authService.getToken().then(() => $location.url('/home'))
+
+    this.signup = function(user) {
+      $log.debug('signupCtrl.signup()');
+
+      authService.signup(user).then(() => $location.url('/home'));
+    }
+  }
+}
